29b5f9e73101ff4bf532e02e9c2c1470e8dedc16
MaurZac <maur.mx@gmail.com>
2024-10-10 13:13:53 -0600

    Initial Commit

---
+++ Walmart_Mauricio_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Walmart_Mauricio_Test
+//
+//  Created by MaurZac on 10/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
